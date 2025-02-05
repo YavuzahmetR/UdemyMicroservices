0988f2e29e82615c9c76453535a596a8127d83c6
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2025-02-05 03:01:52 +0300

    Get All Categories Query

---
+++ UdemyMicroservice.Catalog.Api/Features/Categories/GetAll/GetAllCategoryQueryEndPoint.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UdemyMicroservice.Catalog.Api.Repositories;
+using UdemyMicroservices.Shared;
+using UdemyMicroservices.Shared.Extensions;
+
+namespace UdemyMicroservice.Catalog.Api.Features.Categories.GetAll
+{
+    public sealed record GetAllQueryRequest : IRequestByServiceResult<List<GetAllQueryResponse>>;
+
+    public sealed class GetAllQueryHandler(AppDbContext dbContext, IMapper mapper) : 
+        IRequestHandler<GetAllQueryRequest, ServiceResult<List<GetAllQueryResponse>>>
+    {
+        public async Task<ServiceResult<List<GetAllQueryResponse>>> Handle(GetAllQueryRequest request, CancellationToken cancellationToken)
+        {
+            var categories = await dbContext.Categories.ToListAsync(cancellationToken);
+            var response = mapper.Map<List<GetAllQueryResponse>>(categories); //Bind the categories to the response
+            return ServiceResult<List<GetAllQueryResponse>>.SuccessAsOk(response); //200 OK
+        }
+    }
+    public sealed record GetAllQueryResponse(Guid Id, string Name);
+
+    public static class GetAllCategoryQueryEndPoint 
+    {
+        public static RouteGroupBuilder GetAllCategoryGroupItemEndPoint(this RouteGroupBuilder routeGroupBuilder)
+        {
+            routeGroupBuilder.MapGet("/", async (IMediator mediator) =>
+                (await mediator.Send(new GetAllQueryRequest())).ToGenericResult());
+            return routeGroupBuilder;
+        }
+    }
+}
