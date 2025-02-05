070f411ab8168b98a196236493e9438c7b79bf9e
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2025-02-05 03:03:11 +0300

    Assembly configuration_GlobalUsings done

---
+++ UdemyMicroservice.Catalog.Api/GlobalUsing.cs
@@ -0,0 +1,9 @@
+﻿global using AutoMapper;
+global using MediatR;
+global using Microsoft.EntityFrameworkCore;
+global using UdemyMicroservices.Shared;
+global using UdemyMicroservices.Shared.Extensions;
+global using MassTransit;
+global using System.Net;
+global using FluentValidation;
+global using UdemyMicroservice.Catalog.Api.Repositories;
--- UdemyMicroservice.Catalog.Api/Program.cs
+++ UdemyMicroservice.Catalog.Api/Program.cs
@@ -1,6 +1,8 @@
-using MongoDB.Driver;
+using UdemyMicroservice.Catalog.Api;
+using UdemyMicroservice.Catalog.Api.Features.Categories;
 using UdemyMicroservice.Catalog.Api.Options;
 using UdemyMicroservice.Catalog.Api.Repositories;
+using UdemyMicroservices.Shared.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,9 +10,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddOptExt();
 builder.Services.AddDbServiceExt();
+builder.Services.AddCommonServiceExt(typeof(CatalogAssembly));
+
 
 var app = builder.Build();
 
+app.AddCategoryGroupEndPointExt();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
