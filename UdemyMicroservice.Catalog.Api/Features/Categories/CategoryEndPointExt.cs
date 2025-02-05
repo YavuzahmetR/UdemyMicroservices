7b5af96b1b75fa01e4d20380a3b313837dbaa837
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2025-02-05 03:02:25 +0300

    Grouping Endpoints and AutoMapper

---
+++ UdemyMicroservice.Catalog.Api/Features/Categories/CategoryEndPointExt.cs
@@ -0,0 +1,17 @@
+﻿using UdemyMicroservice.Catalog.Api.Features.Categories.Create;
+using UdemyMicroservice.Catalog.Api.Features.Categories.GetAll;
+using UdemyMicroservice.Catalog.Api.Features.Categories.GetById;
+
+namespace UdemyMicroservice.Catalog.Api.Features.Categories
+{
+    public static class CategoryEndPointExt
+    {
+        public static void AddCategoryGroupEndPointExt(this WebApplication app)
+        {
+            app.MapGroup("api/categories")
+                .CreateCategoryGroupItemEndPoint()
+                .GetAllCategoryGroupItemEndPoint()
+                .GetByIdCategoryGroupItemEndPoint();
+        }
+    }
+}
---
+++ UdemyMicroservice.Catalog.Api/Features/Categories/CategoryMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using UdemyMicroservice.Catalog.Api.Features.Categories.GetAll;
+using UdemyMicroservice.Catalog.Api.Features.Categories.GetById;
+
+namespace UdemyMicroservice.Catalog.Api.Features.Categories
+{
+    public class CategoryMapper : Profile
+    {
+        public CategoryMapper()
+        {
+            CreateMap<Category, GetAllQueryResponse>().ReverseMap();
+            CreateMap<Category, GetByIdQueryResponse>().ReverseMap();
+        }
+    }
+}
