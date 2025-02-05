04b8e9dc3abb8e7f18d53ed01e3f49b198d406a5
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2025-02-05 03:00:21 +0300

    ValidationFilter added

---
+++ UdemyMicroservices.Shared/Filters/ValidationFilter.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace UdemyMicroservices.Shared.Filters
+{
+    public sealed class ValidationFilter<T> : IEndpointFilter
+    {
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+            var validator = context.HttpContext.RequestServices.GetService<IValidator<T>>();
+            //Fast Fail
+            if (validator is null)
+            {
+                return await next(context);
+            }
+            var requestModel = context.Arguments.OfType<T>().FirstOrDefault(); //Get the first argument of type T in the request model
+
+            if (requestModel is null)
+            {
+                return await next(context);
+            }
+
+            var validationResult = await validator.ValidateAsync(requestModel);
+
+            if (!validationResult.IsValid)
+            {
+               return Results.ValidationProblem(validationResult.ToDictionary());
+            }
+
+            // This is the place where we can validate the incoming request - Do something before the request is processed
+            return await next(context);
+        }
+    }
+}
