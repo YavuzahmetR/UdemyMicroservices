d6ae444c7d4baf1eefd0233ec0e70bfa5f76f0de
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2025-02-05 03:01:01 +0300

    CommonServiceExtensions_ResultTypes Done

---
+++ UdemyMicroservices.Shared/Extensions/EndPointResultExt.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyMicroservices.Shared.Extensions
+{
+    public static class EndPointResultExt
+    {
+        public static IResult ToGenericResult<T>(this ServiceResult<T> result) //Generic Version
+        {
+            return result.StatusCode switch
+            {
+                HttpStatusCode.OK => Results.Ok(result.Data),
+                HttpStatusCode.Created => Results.Created(result.UrlAsCreated,result.Data),
+                HttpStatusCode.NoContent => Results.NoContent(),
+                HttpStatusCode.BadRequest => Results.BadRequest(result.Fail),
+                HttpStatusCode.NotFound => Results.NotFound(result.Fail),
+                _ => Results.Problem(result.Fail!) //500 or other
+            };
+        }
+        public static IResult ToGenericResult(this ServiceResult result) //No Generic Version
+        {
+            return result.StatusCode switch
+            {
+                HttpStatusCode.NoContent => Results.NoContent(),
+                HttpStatusCode.BadRequest => Results.BadRequest(result.Fail),
+                HttpStatusCode.NotFound => Results.NotFound(result.Fail),
+                _ => Results.Problem(result.Fail!) //500 or other
+            };
+        }
+    }
+}
